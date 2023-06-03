1ad4da36e198a6cb615ade5e5f492e93249ac82c
Ttoorne <karakozhaev.bektur@gmail.com>
2023-06-03 16:02:38 +0600

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,29 @@
-# Getting Started with Create React App
+# Para Birimi Dönüştürücü Uygulaması
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Bu uygulama, farklı para birimleri arasında dönüşüm yapmanıza olanak sağlar. Kullanıcılar, miktarı ve dönüştürmek istedikleri para birimlerini seçerek dönüşümü gerçekleştirebilirler.
 
-## Available Scripts
+## Nasıl Çalışır
 
-In the project directory, you can run:
+1. Tutar alanına dönüştürmek istediğiniz miktarı girin.
+2. Kaynak para birimini seçmek için "From Currency" dropdown listesini kullanın.
+3. Hedef para birimini seçmek için "To Currency" dropdown listesini kullanın.
+4. "Dönüştür" düğmesine tıklayın.
+5. Sonuç, "Exchange Rate" bölümünde görüntülenecektir.
 
-### `npm start`
+## Gereksinimler
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- Node.js
+- React
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Kurulum
 
-### `npm test`
+1. Bu projeyi klonlayın veya ZIP olarak indirin.
+2. Terminali açın ve proje dizinine gidin.
+3. `npm install` komutunu çalıştırarak bağımlılıkları yükleyin.
+4. `npm start` komutunu çalıştırarak uygulamayı başlatın.
+5. Tarayıcınızda `http://localhost:3000` adresine gidin.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Kaynaklar
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- [React](https://reactjs.org/)
+- [ExchangeRate-API](https://www.exchangerate-api.com/)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const API = "https://api.exchangerate-api.com/v4/latest";
+
+const CurrencyConverter = () => {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("TRY");
+  const [convertedAmount, setConvertedAmount] = useState("");
+
+  const convertCurrency = async (e) => {
+    e.preventDefault();
+    if (!amount.trim()) {
+      alert("Alanı doldurunuz");
+      return;
+    }
+
+    const response = await fetch(`${API}/${fromCurrency}`);
+    const data = await response.json();
+    const rate = data.rates[toCurrency];
+    const result = (parseFloat(amount) * rate).toFixed(2);
+
+    setConvertedAmount(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header>Para Birimi Dönüştürücü</header>
+      <form>
+        <div>
+          <label htmlFor="amount">Tutar</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="drop-list">
+          <div className="from">
+            <label htmlFor="fromCurrency"></label>
+            <div className="select-box">
+              <img
+                src={`https://flagsapi.com/${
+                  fromCurrency[0] + fromCurrency[1]
+                }/flat/64.png`}
+                alt=""
+              />
+              <select
+                id="fromCurrency"
+                value={fromCurrency}
+                onChange={(e) => setFromCurrency(e.target.value)}
+              >
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="TRY">TRY</option>
+                <option value="RUB">RUB</option>
+                <option value="KGS">KGS</option>
+                <option value="JPY">JPY</option>
+                <option value="GBP">GBP</option>
+                <option value="AUD">AUD</option>
+                <option value="CNY">CNY</option>
+                <option value="SAR">SAR</option>
+                <option value="AED">AED</option>
+                <option value="KZT">KZT</option>
+                <option value="UZS">UZS</option>
+              </select>
+            </div>
+          </div>
+          <img
+            className="icon"
+            style={{ width: "30px", height: "30px" }}
+            src="https://freepngimg.com/download/money/48819-5-exchange-png-file-hd.png"
+          />
+          <div className="to">
+            <label htmlFor="toCurrency"></label>
+            <div className="select-box">
+              <img
+                src={`https://flagsapi.com/${
+                  toCurrency[0] + toCurrency[1]
+                }/flat/64.png`}
+                alt=""
+              />
+              <select
+                id="toCurrency"
+                value={toCurrency}
+                onChange={(e) => setToCurrency(e.target.value)}
+              >
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="TRY">TRY</option>
+                <option value="RUB">RUB</option>
+                <option value="KGS">KGS</option>
+                <option value="JPY">JPY</option>
+                <option value="GBP">GBP</option>
+                <option value="AUD">AUD</option>
+                <option value="CNY">CNY</option>
+                <option value="SAR">SAR</option>
+                <option value="AED">AED</option>
+                <option value="KZT">KZT</option>
+                <option value="UZS">UZS</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        {convertedAmount && (
+          <div className="exchange-rate">
+            {amount} {fromCurrency} = {convertedAmount} {toCurrency}
+          </div>
+        )}
+        <button onClick={convertCurrency}>Dönüştür</button>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default CurrencyConverter;
